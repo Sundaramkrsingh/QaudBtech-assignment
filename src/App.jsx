46e475ef9f09fdc0c996d1192384944f49c251e2
Sundaram Singh <sundaramsinghsdnr@gmail.com>
2024-05-05 00:18:44 +0530

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import TaskList from './components/taskList';
 
 function App() {
 
+  // page contains navbar, input component, tasks list-component
   return <div>
-    <Navbar />                                
+    <Navbar />                           
     <div className="min-h-screen grid grid-cols-1 sm:grid-cols-2">
       <TaskInput />
       <TaskList />
--- src/components/taskInput.jsx
+++ src/components/taskInput.jsx
@@ -5,10 +5,12 @@ import { onChangeDuedate, onChangeTitle } from "../redux/slices/todoSlice";
 import { addTodo } from "../redux/slices/todoListSlice";
 
 const TaskInput = () => {
+    // title, dueState state variables
     const title = useSelector((state) => state.todo.title)
     const dueDate = useSelector((state) => state.todo.dueDate)
     const dispatch = useDispatch()
 
+    // onClicking add button todo gets added to list and list gets updated in localStorage
     return <div className="flex justify-center items-center">
         <div className="flex flex-col justify-evenly w-48 sm:w-72 h-60 border-2 border-slate-200 rounded-xl shadow-md hover:shadow-xl px-3 md:px-5 my-3 sm:mb-0"> 
             <InputBox label={"Title"} placeholder={"assignment"} onChange={(e) => dispatch(onChangeTitle(e.target.value))} />
