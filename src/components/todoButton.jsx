6d2cdfa4860f38899e7cca9d00f3055186805b98
Sundaram Singh <sundaramsinghsdnr@gmail.com>
2024-05-05 01:03:59 +0530

    adjusted responsiveness

--- src/components/todoButton.jsx
+++ src/components/todoButton.jsx
@@ -1,6 +1,6 @@
 const TodoButton = ({ onClick, completed }) => {
 
-    return <button className={`text-white rounded-lg ${completed ? 'bg-red-500 focus:bg-red-700 py-2 px-3' : 'bg-green-500 focus:bg-green-700 p-2'}`} onClick={onClick}>
+    return <button className={`text-white ml-1 rounded-lg text-xs sm:text-base ${completed ? 'bg-red-500 focus:bg-red-700 px-2 py-2 sm:py-2 sm:px-3' : 'bg-green-500 focus:bg-green-700 py-2 px-1 sm:p-2'}`} onClick={onClick}>
         {completed? "Add": "Done"}
     </button>
 }
